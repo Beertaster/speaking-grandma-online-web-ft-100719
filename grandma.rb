b4c3618b7cbe6b70f7c89d51e901aefada3b059b
William Stevens <wstevens.hornet@gmail.com>
2019-10-10 06:10:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,10 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma(talk)
+  if talk == talk.upcase
+    return "NO, NOT SINCE 1938!"
+    elsif
+    
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
