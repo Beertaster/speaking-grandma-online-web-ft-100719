6587c67b37abf624def2d953e492e123188a87e7
William Stevens <wstevens.hornet@gmail.com>
2019-10-10 06:14:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(talk)
   if talk == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUPMKIN"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif talk == talk.upcase
     return "NO, NOT SINCE 1938!"
   else
